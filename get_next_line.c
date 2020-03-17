5650a236343e1ca4c30b0ff6113581cb5ad3cde9
StevenKim <stevenkim@Steven.local>
2020-03-17 15:06:44 +0900

    42TESTERS success

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,43 @@
 /*   By: seunkim <seunkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 03:25:34 by seunkim           #+#    #+#             */
-/*   Updated: 2020/03/16 14:07:40 by seunkim          ###   ########.fr       */
+/*   Updated: 2020/03/17 15:00:24 by seunkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int	check_error(char **buff, int fd)
+{
+	if (!(*buff = (char*)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (0);
+	if (fd <= -1 || fd >= 6)
+		return (0);
+	return (1);
+}
+int	getlinestr(char **line, char **data)
+{
+	if (ft_strchr(*data, '\n'))
+	{
+		*line = ft_strndup(*data, ft_strchr(*data, '\n') - *data);
+		*data = ft_strchr(*data, '\n') + 1;
+		return (1);
+	}
+	else
+	{
+		*line = ft_strndup(*data, ft_strchr(*data, '\0') - *data);
+		*data = NULL;
+		return (0);
+	}
+}
 
 int	get_next_line(int fd, char **line)
 {
 	char		*buff;
 	static char	*data;
 	ssize_t		bytes;
-
-	if (!(buff = (char*)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+	
+	if (!(check_error(&buff, fd)))
 		return (-1);
 	if (!data)
 	{
@@ -29,21 +53,7 @@ int	get_next_line(int fd, char **line)
 			data = ft_strjoin(data, buff);
 		}
 	}
-	if (ft_strchr(data, '\n'))
-		*line = ft_strndup(data, ft_strchr(data, '\n') - data);
-	else
-		return (0);
-	data = ft_strchr(data, '\n') + 1;
 	free(buff);
-	return (1);
+	return (getlinestr(&*line, &data));
 }
 
-//int	main(void)
-//{
-//	int	fd;
-//	char	*line;
-
-//	fd = open("text.txt", O_RDONLY);
-//	while (get_next_line(fd, &line))
-//		printf("line = %s\n", line);
-//}
