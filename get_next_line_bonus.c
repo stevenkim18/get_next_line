4b817d8f705d43ec72012d1ef50d372f5a1b6515
StevenKim <stevenkim@Steven.local>
2020-03-18 15:30:34 +0900

    gnl-war-machine success

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: seunkim <seunkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 03:25:34 by seunkim           #+#    #+#             */
-/*   Updated: 2020/03/18 01:26:26 by seunkim          ###   ########.fr       */
+/*   Updated: 2020/03/18 15:02:26 by seunkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	check_error(char **buff, int fd)
 		return (0);
 	return (1);
 }
-int	getlinestr(char **line, char **data)
+int	getlinestr(char **line, char **data, char **first_data)
 {
 	if (ft_strchr(*data, '\n'))
 	{
@@ -33,6 +33,7 @@ int	getlinestr(char **line, char **data)
 	else
 	{
 		*line = ft_strndup(*data, ft_strchr(*data, '\0') - *data);
+		free(*first_data);
 		*data = NULL;
 		return (0);
 	}
@@ -41,21 +42,25 @@ int	getlinestr(char **line, char **data)
 int	get_next_line(int fd, char **line)
 {
 	char		*buff;
-	static char	*data[4096];
+	static char	*data[8192];
+	char		*tmp;
 	ssize_t		bytes;
 	
 	if (!(check_error(&buff, fd)))
 		return (-1);
 	if (!data[fd])
 	{
-		data[fd] = "";
+		data[fd] = ft_strndup("", 0);
 		while ((bytes = read(fd, buff, BUFFER_SIZE)) > 0)
 		{
 			buff[bytes] = '\0';
+			tmp = data[fd];
 			data[fd] = ft_strjoin(data[fd], buff);
+			free(tmp);
 		}
+		data[4096 + fd] = data[fd];
 	}
 	free(buff);
-	return (getlinestr(&*line, &data[fd]));
+	return (getlinestr(&*line, &data[fd], &data[4096 + fd]));
 }
 
